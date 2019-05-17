976e4cd647c6a3f4b9b67a4383bc137c03b503a6
Francesco <francesco@hexbyte.net>
2019-05-17 08:56:34 +0000

    Add header guard to error_helper.h file

--- src/error_helper.h
+++ src/error_helper.h
@@ -1,4 +1,7 @@
+#ifndef ERROR_HELPER_H
+#define ERROR_HELPER_H
 #include <stdio.h>
 #include <stdlib.h>
 
 #define on_error_exit(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
+#endif
